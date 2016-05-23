6474bf8b7f0159f3505425dc8d81a96f60472bd9
Guang <gyang274@gmail.com>
2016-05-23 03:04:08 -0400

    develop skm algorithm

--- wfx/test.r
+++ wfx/test.r
@@ -1,10 +1,38 @@
 # test.r
 
+require(Rcpp)
+require(RcppArmadillo)
+
 sourceCpp("wfx/test1.cpp")
-
-sourceCpp("wfx/test2.cpp")
-
 
 microbenchmark::microbenchmark(
   t1 = seqLenTest1(100), t2 = seqLenTest2(100),
   t3 = seqLenTest3(100), t4 = seqLenTest4(100))
+
+sourceCpp("wfx/test2.cpp")
+
+xx <- new(skmSolution, c(1:10), sample(10), 77.85)
+
+xx$s
+
+xx[["t"]]
+
+xx$o
+
+sourceCpp("wfx/test3.cpp")
+
+mx = matrix(sample(100, 28), 7, 4)
+
+xx = skm_minmax_cpp(mx)
+
+mx = matrix(sample(100, 45), 5, 9)
+
+xx = skm_cpp(mx, c(1, 2, 4), integer(0L), max_it = 10)
+
+xx = skm_cpp(mx, c(1, 2, 4), 0, max_it = 10)
+
+mx = matrix(abs(rnorm(52 * 50000)), 52, 50000)
+
+xx = skm_cpp(mx, 1L:10L, integer(0L), max_it = 1000)
+
+rm(list = setdiff(ls(), "mx"))
