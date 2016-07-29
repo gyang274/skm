35ba19d1e3ac96cc8c2ac31051bb89119777a8a3
Guang Yang <gyang274@gmail.com>
2016-07-29 10:46:32 -0400

    update data documentation

--- R/data.r
+++ R/data.r
@@ -9,9 +9,13 @@
 #------------------------------------------------------------------------------#
 #------------------------------------ data ------------------------------------#
 #------------------------------------------------------------------------------#
-#' zip2012: a zip code database with latitude, longitude, population and income.
+#' zip2012
 #'
-#' @format A data table with 28844 rows and 9 variables:
+#' @description
+#'  a zip code database with latitude, longitude, population and income.
+#'
+#' @format
+#'  A data table with 28844 rows and 9 variables:
 #' \describe{
 #'   \item{zip}{zip code, 5 digits zip code in U.S.}
 #'   \item{lat}{latitude}
@@ -23,13 +27,20 @@
 #'   \item{p_pop}{percentage of population w.r.t total population}
 #'   \item{p_ink}{percentage of income w.r.t total income}
 #' }
+#'
 #' @source \url{http://federalgovernmentzipcodes.us/}
+#'
 "zip2012"
 
-#' source_zip_list: a list of zip code used in skm package demonstration.
+#' source_zip_list
 #'
-#' @format: a character vector of length 51, one 5 digits zip code selected from
-#' each state, selected the most central zip code in each state.
+#' @description
+#'  a list of zip code used in skm package demonstration.
+#'
+#' @format
+#'  a character vector of length 51 includes one 5 digits zip code selected from
+#'  each state, where the most central zip code in each state selected.
+#'
 "source_zip_list"
 #------------------------------------------------------------------------------#
 
